01b269dda83a198c954a16a445280e03eaa3cb61
Arthur Wang <rthrwang@gmail.com>
2017-11-03 10:10:34 +0800

    fixed terminal bugs

--- src/features/logtalkTerminal.ts
+++ src/features/logtalkTerminal.ts
@@ -57,32 +57,33 @@ export default class LogtalkTerminal {
     LogtalkTerminal.createLogtalkTerm();
     LogtalkTerminal._terminal.show(true);
   }
+
   public static loadDocument(uri: Uri) {
     LogtalkTerminal.createLogtalkTerm();
-    const file = jsesc(path.resolve(uri.fsPath));
+    const file: string = LogtalkTerminal.ensureFile(uri);
     let goals = `set_logtalk_flag(report,warnings),ignore(logtalk_load('${file}')).`;
     LogtalkTerminal.sendString(goals);
   }
 
   public static runUnitTest() {
     LogtalkTerminal.createLogtalkTerm();
-    const file = jsesc(path.join(workspace.rootPath, "tester"));
+    const file = path.join(workspace.rootPath, "tester");
     let goals = `ignore(logtalk_load('${file}')).`;
     LogtalkTerminal.sendString(goals);
   }
 
   public static runDoclet() {
     LogtalkTerminal.createLogtalkTerm();
-    const file = jsesc(path.join(workspace.rootPath, "doclet"));
+    const file = path.join(workspace.rootPath, "doclet");
     let goals = `logtalk_load(doclet(loader)),ignore(logtalk_load('${file}')).`;
     LogtalkTerminal.sendString(goals);
   }
 
   public static genHtmlDoc(uri: Uri) {
     LogtalkTerminal.createLogtalkTerm();
-    const file = jsesc(path.resolve(uri.fsPath));
-    const dir = jsesc(path.dirname(uri.fsPath));
-    const xmlDir = jsesc(path.join(dir, "xml_docs"));
+    const file: string = LogtalkTerminal.ensureFile(uri);
+    const dir = path.dirname(file);
+    const xmlDir = path.join(dir, "xml_docs");
     let goals = `logtalk_load(lgtdoc(loader)),logtalk_load('${file}'),os::change_directory('${dir}'),lgtdoc::directory('${dir}').`;
     LogtalkTerminal.sendString(goals);
     cp.execSync("lgt2html && code index.html", { cwd: xmlDir });
@@ -90,8 +91,8 @@ export default class LogtalkTerminal {
 
   public static genSVGDiagrams(uri: Uri) {
     LogtalkTerminal.createLogtalkTerm();
-    const file = jsesc(path.resolve(uri.fsPath));
-    const dir = jsesc(path.dirname(uri.fsPath));
+    const file: string = LogtalkTerminal.ensureFile(uri);
+    const dir = path.dirname(file);
     let goals = `logtalk_load(diagrams(loader)),logtalk_load('${file}'),os::change_directory('${dir}'),diagrams::directory('${dir}').`;
     LogtalkTerminal.sendString(goals);
     cp.execSync(
@@ -101,8 +102,18 @@ export default class LogtalkTerminal {
   }
   public static scanForDeadCode(uri: Uri) {
     LogtalkTerminal.createLogtalkTerm();
-    const file = jsesc(path.resolve(uri.fsPath));
+    const file: string = LogtalkTerminal.ensureFile(uri);
     let goals = `set_logtalk_flag(report, warnings),ignore(logtalk_load('${file}')),flush_output,logtalk_load(dead_code_scanner(loader)),dead_code_scanner::all.`;
     LogtalkTerminal.sendString(goals);
+  }
+
+  private static ensureFile(uri: Uri): string {
+    let file: string;
+    if (uri && uri.fsPath) {
+      file = uri.fsPath;
+    } else {
+      file = window.activeTextEditor.document.fileName;
+    }
+    return file;
   }
 }
